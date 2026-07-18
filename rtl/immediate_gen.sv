9f4ed5b933afbfb3436c35273609f60c04440959
Andrew Tsomik <andrew.tsomik@gmail.com>
2026-07-18 18:26:50 -0400

    Immediate generator built for now, will be changed later as needed

--- rtl/immediate_gen.sv
+++ rtl/immediate_gen.sv
@@ -1,9 +1,22 @@
+import risc_pkg::*;
+
 module immediate_gen(
 	input logic [31:0] instruct,
 	output logic[31:0] imm_out	
 );
 
-always_comb begin
+opcodes_e opcode;
+assign opcode = opcodes_e'(instruct[6:0]);
 
+always_comb begin
+	imm_out = 32'd0;
+	case(opcode)
+		OP : imm_out = 32'd0;
+		OP_IMM, LOAD : imm_out = {{20{instruct[31]}}, instruct[31:20]};
+		STORE : imm_out = {{20{instruct[31]}}, instruct[31:25], instruct[11:7]};
+		BRANCH : imm_out = {{20{instruct[31]}}, instruct[7], instruct[30:25], instruct[11:8], 1'b0};
+		JAL : imm_out = {{12{instruct[31]}}, instruct[19:12], instruct[20], instruct[30:21], 1'b0};
+	endcase
+end
 
 endmodule 
