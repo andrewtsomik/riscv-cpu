c0f9f77dea3024c1c8a8b9f8439f0239472b839a
Andrew Tsomik <andrew.tsomik@gmail.com>
2026-07-19 13:51:57 -0400

    Built instruction memory and test hexs

---
+++ rtl/instruct_mem.sv
@@ -0,0 +1,14 @@
+module instruct_mem(
+	input logic [31:0] addr,
+	output logic [31:0] instruct
+);
+	logic [31:0] arr [0:31];
+
+	initial begin
+		for(int i = 0; i < 32; i++)
+			arr[i] = 32'd0;
+		$readmemh("../programs/test.hex", arr);	
+	end
+
+	assign instruct = arr[addr[6:2]];
+endmodule
