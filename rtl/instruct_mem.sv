eb6ed21a39d18203bad1955e39e63d3bedd95d02
Andrew Tsomik <andrew.tsomik@gmail.com>
2026-07-21 22:05:18 -0400

    Ok everything is done now the whole instruction set is complete

--- rtl/instruct_mem.sv
+++ rtl/instruct_mem.sv
@@ -2,13 +2,13 @@ module instruct_mem(
 	input logic [31:0] addr,
 	output logic [31:0] instruct
 );
-	logic [31:0] arr [0:31];
+	logic [31:0] arr [0:1023];
 
 	initial begin
-		for(int i = 0; i < 32; i++)
+		for(int i = 0; i < 1024; i++)
 			arr[i] = 32'd0;
 		$readmemh("../programs/shift.hex", arr);	
 	end
 
-	assign instruct = arr[addr[6:2]];
+	assign instruct = arr[addr[11:2]];
 endmodule
