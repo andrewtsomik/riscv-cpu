ff85ae8a82c84db12b21ddfbede88be965b6930d
Andrew Tsomik <andrew.tsomik@gmail.com>
2026-07-22 20:05:10 -0400

    Added packed structs for pipelining

--- rtl/risc_pkg.sv
+++ rtl/risc_pkg.sv
@@ -23,4 +23,44 @@ package risc_pkg;
 		ALU_SRL = 4'd8,
 		ALU_SRA = 4'd9
 	} alu_ops_e;
+
+	typedef struct packed {
+		logic [31:0] instruct;
+		logic [31:0] pc;
+		logic [31:0] pcplus4;
+	} if_id_t;
+
+	typedef struct packed {
+		logic [31:0] imm_out;
+		logic [4:0] rd_addr1;
+		logic [4:0] rd_addr2;
+		logic [4:0] wr_addr;
+		logic [31:0] rd_dt1;
+		logic [31:0] rd_dt2;
+		logic alu_src, alu_a, mem_rd, mem_wr, reg_wr, branch, jump;
+		logic [1:0] mem_to_reg;
+		logic [31:0] pc;
+		logic [31:0] pcplus4;
+		alu_ops_e alu_op;
+	} id_ex_t;
+
+	typedef struct packed {
+		logic [31:0] alu_result;
+		logic [31:0] rd_dt2;
+		logic mem_rd, mem_wr, reg_wr;
+		logic [1:0] mem_to_reg;
+		logic [4:0] wr_addr;
+		logic [31:0] pcplus4;
+		logic [31:0] imm_out;
+	} ex_mem_t;
+
+	typedef struct packed {
+		logic [31:0] imm_out;
+		logic [31:0] pcplus4;
+		logic [31:0] mem_rd_dt;
+		logic [31:0] alu_result;
+		logic [4:0] wr_addr;
+		logic [1:0] mem_to_reg;
+		logic reg_wr;
+	} mem_wb_t;
 endpackage
